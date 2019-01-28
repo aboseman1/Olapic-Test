a275971ea4b3dd37f954e29e0cecd464aea255dc
Andrew <aboseman@yext.com>
2019-01-27 21:37:14 -0500

    Commit to retrieve the URLs for the images

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ $(function () {
     url: "https://photorankapi-a.akamaihd.net/customers/215757/media/recent?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2&count=5",
     success: function(response) {
       console.log(response.data._embedded.media)
-      let media = response.data._embedded.media
-      console.log(media)
+      let mediaJSON = response.data._embedded.media;
+      console.log(mediaJSON)
+      $.each(mediaJSON, function(i, image) {
+        console.log(mediaJSON[i].images.normal);
+      }
+    )
     }
   });
 });
