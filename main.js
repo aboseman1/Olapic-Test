9ac1e61295982f7f374723b3637b250a509bd112
Andrew <aboseman@yext.com>
2019-01-27 21:22:55 -0500

    Commit to parse out the Image section of the response

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 $(function () {
   $.ajax({
     type: 'GET',
-    url: 'https://photorankapi-a.akamaihd.net/customers/215757/media/recent?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2&count=5'
+    url: "https://photorankapi-a.akamaihd.net/customers/215757/media/recent?auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2&count=5",
     success: function(response) {
-      
+      console.log(response.data._embedded.media)
+      let media = response.data._embedded.media
+      console.log(media)
     }
-  })
+  });
+});
